d8803080dab22099ee7f0550ccd394c058a12ef3
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-10-23 17:41:55 -0400

    added addDept and addRole

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cTable = require('console.table');
 // let allRoles =['Sales Lead', 'Salesperson', 'Engineering Manager', 'Software Engineer', 'Account Manager',
 // 'Accountant', 'HR Manager', 'HR Specialist'];
 let allRoles =[];
+let allDepts = [];
 
 const db = mysql.createConnection(
     {
@@ -37,13 +38,15 @@ const mainMenu = () => {
             rolesQuery()
         } else if (data.menu === 'Add Department') {
             addDept()
+        } else if (data.menu === 'Add Role') {
+            deptQuery()
         }
     })
 }
 
 const viewDepartments = () => {
     db.query('SELECT * FROM departments', function (err, results) {
-        console.table(results);
+        console.table('\n',results);
         mainMenu()
       });
 }
@@ -52,28 +55,23 @@ const viewRoles = () => {
     db.query(`SELECT employee_role.id, employee_role.title ,departments.dept_name, employee_role.salary
         FROM departments
         JOIN employee_role ON employee_role.department_id=departments.id`, function (err, results) {
-            console.table(results);
+            console.table('\n',results);
             mainMenu()
           });
 }
 
 const viewEmployees = () => {
-    db.query(`SELECT employees.id, employees.first_name, employees.last_name, departments.dept_name, employee_role.salary
+    db.query(`SELECT employees.id, employees.first_name, employees.last_name, employee_role.title, departments.dept_name, employee_role.salary
         FROM employees
         JOIN employee_role ON employee_role.id=employees.role_id
         JOIN departments ON departments.id=employee_role.department_id`, function(err, results) {
-            console.table(results);
+            console.table('\n',results);
             mainMenu()
         })
 }
 
 const addEmployee = () => {
     inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Enter employee id'
-        },
         {
             type: 'input',
             name: 'firstName',
@@ -97,16 +95,18 @@ const addEmployee = () => {
         }
     ])
     .then((data) => {
-        let newId = data.id;
         let newFirstName = data.firstName;
         let newLastName = data.lastName;
         let newManager = data.manager
-        db.query(`SELECT id FROM employee_role WHERE title = ?`, data.role, function(err, results) {
-            let values = `${newId}, '${newFirstName}', '${newLastName}', ${results[0].id}, ${newManager}`;
-            console.log(values);
-            db.query(`INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES (${values})`, function(err, result) {
-                console.log('\nNew Employee Added!\n')
-                mainMenu();
+        db.query(`SELECT id FROM employees`, (err, results) => {
+            let newId = results.length;
+            newId++;
+            db.query(`SELECT id FROM employee_role WHERE title = ?`, data.role, function(err, results) {
+                let values = `${newId}, '${newFirstName}', '${newLastName}', ${results[0].id}, ${newManager}`;
+                db.query(`INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES (${values})`, function(err, result) {
+                    console.log('\nNew Employee Added!\n')
+                    mainMenu();
+                })
             })
         })
     })
@@ -121,20 +121,66 @@ const addDept = () => {
         }
     )
     .then((data) => {
-        db.query(`INSERT INTO departments (dept_name) VALUES (${data.dept})`, (err, results) => {
+        console.log(data.dept)
+        db.query(`INSERT INTO departments (dept_name) VALUES ('${data.dept}')`, (err, results) => {
             console.log('\nDepartment added!\n')
             mainMenu()
         })
     })
 }
 
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter job title'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter job salary'
+        },
+        {
+            type: 'list',
+            name: 'dept',
+            message: 'Enter job department',
+            choices: allDepts
+        }
+    ])
+    .then((data) => {
+        let title = data.title;
+        let salary = data.salary;
+        db.query(`SELECT title FROM employee_role`, (err, results) => {
+            let roleId = results.length;
+            roleId++;
+            db.query(`SELECT id FROM departments WHERE dept_name = ?`, data.dept, function(err, results) {
+                let values = `${roleId}, '${title}', ${salary}, ${results[0].id}`
+                db.query(`INSERT INTO employee_role (id, title, salary, department_id) VALUES (${values})`, (err, results) => {
+                    console.log('\nRole Added!\n');
+                    mainMenu();
+                })
+            })
+        })
+    })
+}
+
 const rolesQuery = () => {
-db.query(`SELECT title FROM employee_role`, (err, results) => {
-    for(let i = 0; i < results.length; i++) {
-    allRoles.push(results[i].title)
-    }
-addEmployee()
-})
+    db.query(`SELECT title FROM employee_role`, (err, results) => {
+        for(let i = 0; i < results.length; i++) {
+            allRoles.push(results[i].title)
+        }
+        addEmployee()
+    })
+}
+
+const deptQuery = () => {
+    db.query(`SELECT dept_name FROM departments`, (err, results) => {
+        for(let i = 0; i < results.length; i++){
+            allDepts.push(results[i].dept_name)
+        }
+        addRole()
+    })
 }
 
 
